3e2d2fa7e6ae4965b6921e13bb9f83c29806806e
lboyz <98627557+lboyz@users.noreply.github.com>
2024-05-01 00:50:36 +0700

    Delete resources directory

--- resources/views/components/menu-items.blade.php
+++
@@ -1,9 +0,0 @@
-
-<div  style="width:500px; display:flex; align-items:center">
-  <img src="{{$image}}" style="position:relative; width: 290px; object-fit:contain; z-index:20" alt="">
-  <div class="py-3 px-5 rounded" style="background: white; position:relative; left:-8.5em"> 
-    <h5 style="color: #29B066; margin-left:2.5em;">{{$title}}</h5>
-    <p style="width: 161px; margin-left:5em">{{$desc}}</p>
-    <a href="/outlet"><p style="color: #B02929;margin-left:5em;">Lihat {{$jumlah}} outlet ></p></a>
-  </div>
-</div>
--- resources/views/components/outlet-buttons.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="outletButtons">
-  {{$slot}}
-</div>
